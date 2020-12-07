b2155a1ace572c3929039067e465ac749041a0fa
Francisco Tapia Trabajo <ftapia@mph.cl>
2020-12-07 12:11:43 -0300

    Mejorada seguridad de SETTINGS mediante el uso de variables de entorno en archivo .env

--- djangoTutorial/settings.py
+++ djangoTutorial/settings.py
@@ -10,17 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from dotenv import load_dotenv
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Declaración de La ubicación del archivo .env
+load_dotenv(dotenv_path=BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u$ef6tljk6jsjof78325ffz+4c9j!2ijqm@22^m%&8ahxu6)3*'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
